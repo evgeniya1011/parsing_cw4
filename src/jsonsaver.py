de3750cf92208e0247a0f556b72bc30d00ad32b3
evgeniya1011 <zhdavydova@mail.ru>
2023-07-18 16:39:44 +0500

    add jsonsaver

---
+++ src/jsonsaver.py
@@ -0,0 +1,34 @@
+from abc import ABC, abstractmethod
+import json
+class JSONAbc(ABC):
+
+    @abstractmethod
+    def add_vacancy(self, vacancy):
+        pass
+
+    @abstractmethod
+    def delete_vacancy(self, vacancy):
+        pass
+
+
+class JSONSaver(JSONAbc):
+
+    def add_vacancy(self, vacancies):
+        data = []
+        for vacancy in vacancies:
+            vacancy_in_dict = {
+                "название": vacancy.title,
+                "ссылка": vacancy.url,
+                "зарплата": vacancy.salary,
+                "требования": vacancy.requirements,
+            }
+            data.append(vacancy_in_dict)
+        with open("../src/vacancy.json", "w", encoding="utf-8") as file:
+            json.dump(data, file, ensure_ascii=False, indent=2)
+
+    def delete_vacancy(self, vacancy):
+        with open("../src/vacancy.json", "r", encoding="utf-8") as file:
+            vacancies = json.load(file)
+            new_vacancies = [vac for vac in vacancies if vacancy not in vac.values()]
+        with open("../src/vacancy.json", "w", encoding="utf-8") as file:
+            json.dump(new_vacancies, file, ensure_ascii=False, indent=2)
